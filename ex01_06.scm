ae2beeca941c391cb7fd40036bf73a8f8e5bb9a4
leiming <leiming@Mac.local>
2015-04-08 00:46:32 +0800

    init

---
+++ ex01_06.scm
@@ -0,0 +1,34 @@
+;;; http://sicp.readthedocs.org/en/latest/chp1/6.html
+
+(define (square x)
+	(* x x)
+)
+
+(define (sqrt-iter guess x)
+	(if (improve-good-enough? guess (improve guess x))
+		(improve guess x)
+		(sqrt-iter (improve guess x) x)
+	)
+)
+
+(define (improve guess x)
+	(avarange guess (/ x guess))
+)
+
+(define (avarange x y)
+	(/ (+ x y) 2)
+)
+
+(define (good-enough? guess x)
+	(< (abs (- (square guess) x)) 0.01)
+)
+
+(define (improve-good-enough? guess new-guess)
+	(< (abs (- 1 (/ guess new-guess))) 0.001) 
+)
+
+(define (sqrt x)
+	(sqrt-iter 1.0 x)
+)
+
+(display (sqrt 0.00009))
