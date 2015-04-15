d586930e8494d505670e146286714c401389732c
leiming <leiming@Mac.local>
2015-04-15 21:09:04 +0800

    finish ex01_12.scm

---
+++ ex01_12.scm
@@ -0,0 +1,12 @@
+(define (pascal-rec i j)
+  (cond ((= 1 j) 1)
+        ((= i j) 1)
+        ((> j i) 0)
+        (else (+ (pascal-rec (- i 1) j )
+                 (pascal-rec (- i 1) (- j 1))))))
+
+(display (pascal-rec 5 3))
+(newline)
+
+(display (pascal-rec 6 4))
+(newline)
