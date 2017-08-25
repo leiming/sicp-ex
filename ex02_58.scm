4b80a3da00bbd406b5dc7347b89e544063939b28
leiming <poetcoders@gmail.com>
2017-08-26 04:04:36 +0800

    Better!

--- ex02_58.scm
+++ ex02_58.scm
@@ -6,3 +6,29 @@
 ; 若是乘号，加括号，返回
 ; 继续遍历 
 ; 晚安 ~
+
+(define (add-brace expression)
+  (define (add-brace-iter before exp after)
+    ;(displayln before)
+    ;(displayln exp)
+    ;(displayln after)
+    ;(displayln "--------------")
+    (let ((s1 (car exp))
+          (oper (cadr exp)))
+      (cond ((empty? (cdr after)) (append before (list (append exp after)))) ;; 除了空值判断以外，加符号判断
+            ((eq? oper '+)
+             (add-brace-iter (append before exp) (list (car after) (cadr after)) (cddr after)))
+            ((eq? oper '*)
+             (add-brace-iter before (list (append exp (list (car after))) (cadr after)) (cddr after)))
+            (else
+              (error "111")))))
+  (add-brace-iter '() (list (car expression) (cadr expression)) (cddr expression)))
+
+(add-brace (list 1 '+ 2 '* 3 '+ 4 '* 5 '+ 6))
+; '(1 + (2 * 3) + (4 * 5) + 6)
+(add-brace (list 1 '+ 2 '* 3 '* 4 '* 5 '+ 6))
+; '(1 + (((2 * 3) * 4) * 5) + 6)
+(add-brace (list 1 '+ 2 '* 3 '* 4 '* 5 '* 6))
+; '(1 + ((((2 * 3) * 4) * 5) * 6))
+(add-brace (list 1 '* 2 '* 3 '* 4 '* 5 '+ 6))
+; '((((((1 * 2) * 3) * 4) * 5) + 6)) 这个多一层括号，再改，晚安 
